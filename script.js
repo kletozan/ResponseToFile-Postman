db573c1346d5277e54a823289f19932262be1daa
Kleto Zan <kleto.zan@zwift.com>
2024-08-30 10:39:59 -0300

    new /write endpoint to create protobuf file

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
+
 const express = require('express'),
-  app = express(),
-  fs = require('fs'),
-  shell = require('shelljs'),
+    app = express(),
+    fs = require('fs'),
+    path = require('path'),
+    shell = require('shelljs'),
+    dateFormat = require("dateformat"),
+    bodyParser = require('body-parser'),
+    byteSize = require('byte-size'),
+    homedir = require('os').homedir(),
+    folderPath = path.join(homedir, 'Postman', 'files');
+    defaultFileExtension = 'txt',
+    FS_DEFAULT_MODE = 'writeFile',
+    DATE_FORMAT ='[yyyy-mm-dd HH:MM:ss]';
 
-   // Modify the folder path in which responses need to be stored
-  folderPath = './Responses/',
-  defaultFileExtension = 'json', // Change the default file extension
-  bodyParser = require('body-parser'),
-  DEFAULT_MODE = 'writeFile',
-  path = require('path');
-
-// Create the folder path in case it doesn't exist
 shell.mkdir('-p', folderPath);
-
- // Change the limits according to your response size
-app.use(bodyParser.json({limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); 
 
 app.get('/', (req, res) => res.send('Hello, I write data to file. Send them requests!'));
 
-app.post('/write', (req, res) => {
-  let extension = req.body.fileExtension || defaultFileExtension,
-    fsMode = req.body.mode || DEFAULT_MODE,
-    uniqueIdentifier = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
-    filename = `${req.body.requestName}${uniqueIdentifier || ''}`,
+app.get('/read/:filename', (req, res) => {
+    filePath = `${path.join(folderPath, req.params.filename)}`;
+    const content = fs.readFileSync(filePath);
+    res.send(content);
+});
+
+app.post('/write', bodyParser.json({limit: '1mb'}), (req, res) => {
+    let extension = req.body.fileExtension || defaultFileExtension,
+    fsMode = req.body.mode || FS_DEFAULT_MODE,
+    filename = req.body.uniqueIdentifier ? typeof req.body.uniqueIdentifier === 'boolean' ? Date.now() : req.body.uniqueIdentifier : false,
     filePath = `${path.join(folderPath, filename)}.${extension}`,
     options = req.body.options || undefined;
 
-  fs[fsMode](filePath, req.body.responseData, options, (err) => {
-    if (err) {
-      console.log(err);
-      res.send('Error');
+    var buffer = undefined;
+    if (req.body.responseData.type == 'Buffer') {
+        buffer = Buffer.from(req.body.responseData);
+    } else {
+        console.log(dateFormat(Date.now(), DATE_FORMAT) + ' ERROR Invalid responseData.type ' + req.body.responseData.type);
+        res.send('Error');
+        return;
     }
-    else {
-      res.send('Success');
-    }
-  });
+
+    fs[fsMode](filePath, buffer, options, (err) => {
+        if (err) {
+            console.log(err);
+            res.send('Error');
+        } else {
+            var postmanToken = req.get('Postman-Token');
+            if (postmanToken) {
+                postmanTokenFormat = ' ' + postmanToken;
+            }
+            console.log(dateFormat(Date.now(), DATE_FORMAT) + ' FILE ' + filePath + postmanTokenFormat + ' (' + byteSize(buffer.length) + ')');
+            res.send('Success');
+        }
+    });
 });
 
-app.listen(3000, () => {
-  console.log('ResponsesToFile App is listening now! Send them requests my way!');
-  console.log(`Data is being stored at location: ${path.join(process.cwd(), folderPath)}`);
+var server = app.listen(3000, "localhost", () => {
+    console.log(dateFormat(Date.now(), DATE_FORMAT) + 
+        ' ResponsesToFile listening to http://' + server.address().address + ':' + server.address().port);
+    console.log(dateFormat(Date.now(), DATE_FORMAT) + ' Files stored at ' + `${folderPath}`);
 });
